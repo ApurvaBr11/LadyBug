40f40a78fe3fd47899ca6fa6b4f4ecdd6838b5d3
apurvaBr11 <apurvkashyappurnea@gmail.com>
2023-12-26 11:03:09 +0530

    Spline-Model-Thunder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Thunder from '@/components/Thunder'
 
-const home = () => {
+const homepage = () => {
   return (
     <div className="min-h-screen w-full bg-black flex justify-center items-center">
       <div className="w-full text-white text-7xl p-8 font-bold">
@@ -15,4 +15,4 @@ const home = () => {
   )
 }
 
-export default home
+export default homepage
