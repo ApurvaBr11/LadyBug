48951e162b5168afaf440f3820065d7d2b0cf6f9
apurvaBr11 <apurvkashyappurnea@gmail.com>
2023-12-28 19:36:08 +0530

    Spline-Model-Thunder

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
+import Color from "@/components/Colors";
 import Thunder from "@/components/Thunder";
 
 const homepage = () => {
   return (
-    <div className="min-h-screen w-full bg-black flex justify-center items-center">
+    <div className="min-h-screen w-full bg-black flex justify-center items-center px-32">
       <div className="w-full text-white text-7xl p-8 font-bold">
         <p>If</p>
         <p>You dont feel the blaze</p>
       </div>
       <div className=" h-screen w-full flex justify-end items-end">
         <Thunder />
+        {/* <Color/> */}
       </div>
     </div>
   );
---
+++ components/Colors.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Color() {
+  return (
+    <Spline scene="https://prod.spline.design/II1LzmKK8LOt7Lbw/scene.splinecode" />
+  );
+}
